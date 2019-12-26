c057b2a270e020e9445fa8024b63773682bfadf5
vedha98 <vetha.gnanam98@gmail.com>
2019-12-26 19:36:21 +0530

    cubes animation

--- main.js
+++ main.js
@@ -2,70 +2,88 @@
 var cards = [
     {
         img: './src/cashier.min.svg',
-        title: "hello",
-        desc: 'asdasdasd',
+        title: "Vapor",
+        desc: 'Serverless Platform',
         bgcolor:"#50C5F3"
     },
     {
         img: './src/dusk.min.svg',
-        title: "hello",
-        desc: 'asdasdasd',
+        title: "Forge",
+        desc: 'Server Management',
         bgcolor:'#54B886'
     },
     {
         img: './src/echo.min.svg',
-        title: "hello",
-        desc: 'asdasdasd',
+        title: "Envoyer",
+        desc: 'Zero Downtime Payment',
         bgcolor:'#EE6656'
     },
     {
         img: './src/horizon.min.svg',
-        title: "hello",
-        desc: 'asdasdasd',
+        title: "Horizon",
+        desc: 'Queue Monitoring',
         bgcolor:'#8C6ED3'
     },
     {
         img: './src/homestead.min.svg',
-        title: "hello",
-        desc: 'asdasdasd',
+        title: "Nova",
+        desc: 'Administration Panel',
         bgcolor:'#4099DE'
     },
     {
         img: './src/forge.min.svg',
-        title: "hello",
-        desc: 'asdasdasd',
+        title: "Echo",
+        desc: 'Realtime Events',
         bgcolor:'#4DB2B0'
     },
     {
         img: './src/echo.min.svg',
-        title: "hello",
-        desc: 'asdasdasd',
+        title: "Lumen",
+        desc: 'Micro Framework',
         bgcolor:'#F5AE7A'
     },
     {
         img: './src/envoyer.min.svg',
-        title: "hello",
-        desc: 'asdasdasd',
+        title: "Homestead",
+        desc: 'Pre Packaged Vagrant box',
         bgcolor:'#E78020'
     },
     {
         img: './src/dusk.min.svg',
-        title: "hello",
-        desc: 'asdasdasd',
+        title: "Spark",
+        desc: 'saas app scafolding',
         bgcolor:'#F0C376'
     },
 ]
 window.onload = () => {
     cards.forEach(val => {
-        var element = `<div class="eco-card">
-       <div class="card-img" style='background-color:${val.bgcolor}'>
-           <img height="100%" src="${val.img}" alt="">
-       </div>
-       <div class="card-details">
-          ${val.title}
-          <span>${val.desc}</span>
-       </div>
-   </div>`
-        document.getElementById("eco-card-grid").innerHTML = document.getElementById("eco-card-grid").innerHTML + element
+        var element = document.createElement('div')
+        element.classList.add("eco-card");
+        var imgchild = document.createElement('div');
+        imgchild.classList.add("card-img")
+        imgchild.style.backgroundColor= val.bgcolor;
+        var img = document.createElement('img')
+        img.src= val.img;
+        imgchild.appendChild(img);
+        var carddetails = document.createElement('div');
+        carddetails.classList.add('card-details');
+        carddetails.innerText=(val.title);
+        var cardspan = document.createElement('span');
+        cardspan.innerText= val.desc;
+        carddetails.appendChild(cardspan)
+        element.appendChild(imgchild);
+        element.appendChild(carddetails) 
+
+//         var element = `<div class="eco-card">
+//        <div class="card-img" style='background-color:${val.bgcolor}'>
+//            <img height="100%" src="${val.img}" alt="">
+//        </div>
+//        <div class="card-details">
+//           ${val.title}
+//           <span>${val.desc}</span>
+//        </div>
+//    </div>`
+        // document.getElementById("eco-card-grid").innerHTML = document.getElementById("eco-card-grid").innerHTML + element
+        document.getElementById("eco-card-grid").appendChild(element);
     })
 }
